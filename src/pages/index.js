34b549ea0c8bc73cf210c4406599075ce89991c3
Brunya <zbruno.linux@gmail.com>
2020-03-04 20:57:20 +0100

    disableAutoScroll-in-mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
-import React from "react"
-import { Link } from "gatsby"
-import Fullpage, { FullPageSections, FullpageSection, FullpageCount, FullpageNavigation } from '@ap.cx/react-fullpage';
+import React, { useEffect, useState } from "react"
+import Fullpage, { FullPageSections, FullpageSection, FullpageNavigation } from '@ap.cx/react-fullpage';
 import { Helmet } from "react-helmet";
 
 import "../components/Style.css"
@@ -11,39 +10,63 @@ import Projects from "../components/Projects"
 import Partners from "../components/Partners"
 import Contact from "../components/Contact"
 
-const IndexPage = () => (
-  <>
-  <Helmet>
-       <title>Zawiasa.hu</title>
-       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css"/>
-   </Helmet>
 
-  <Fullpage>
-    <FullpageNavigation itemStyle={{cursor: "pointer", background: "white", borderRadius: "0%"}} transitionTiming={2000}/>
-    <FullPageSections>
-      <FullpageSection>
-        <Head/>
-      </FullpageSection>
+function IndexPage() {
 
-      <FullpageSection>
-        <Team/>
-      </FullpageSection>
+  const isDesktop = window.outerWidth > 768;
+  console.log(window.outerWidth);
 
-      <FullpageSection>
-        <Projects/>
-      </FullpageSection>
+    return (
+      <>
+      <Helmet>
+           <title>Zawiasa.hu</title>
+           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css"/>
+      </Helmet>
 
-      <FullpageSection>
-        <Partners/>
-      </FullpageSection>
-
-      <FullpageSection>
-        <Contact/>
-      </FullpageSection>
-
-    </FullPageSections>
-  </Fullpage>
-  </>
-)
+       {isDesktop ? (
+          <>
+           <Fullpage>
+             <FullpageNavigation itemStyle={{cursor: "pointer", background: "white", borderRadius: "0%"}} transitionTiming={2000}/>
+             <FullPageSections>
+               <FullpageSection>
+                 <Head/>
+               </FullpageSection>
+               <FullpageSection>
+                 <Team/>
+               </FullpageSection>
+               <FullpageSection>
+                 <Projects/>
+               </FullpageSection>
+               <FullpageSection>
+                 <Partners/>
+               </FullpageSection>
+               <FullpageSection>
+                 <Contact/>
+               </FullpageSection>
+             </FullPageSections>
+           </Fullpage>
+           </>
+          ) : (
+            <>
+             <div className="page">
+               <Head/>
+             </div>
+             <div className="page">
+               <Team/>
+             </div>
+             <div className="page">
+               <Projects/>
+             </div>
+             <div className="page">
+               <Partners/>
+             </div>
+             <div className="page">
+               <Contact/>
+             </div>
+             </>
+          )}
+      </>
+    )
+}
 
 export default IndexPage
